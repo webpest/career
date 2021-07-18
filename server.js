8549d1543271bb22178f531a2faa892627228e12
Abayomi Oloyinde <webpest@gmail.com>
2021-07-18 21:28:13 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,28 @@
+const fastify = require("fastify")({ logger: false });
+const PORT = 3000;
+
+// Setup our static files
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// fastify-formbody lets us parse incoming forms
+fastify.register(require("fastify-formbody"));
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+// Run the server and report out to the logs
+fastify.listen(PORT, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
+});
